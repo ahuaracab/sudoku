3afa5eac438d43fd3d47b7c62975a71e9151874e
Angelo Huaraca <ahuaracab@gmail.com>
2024-01-18 01:22:22 -0500

    update deps

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -1,23 +1,22 @@
 /// <reference types="cypress"/>
 
-describe('Timer', () => {
-    it('shows 10 seconds', () => {
-        cy.visit('/')
-        for (let j = 0; j < 10; j++) {
-            cy.contains('.status__time', `00:0${j}`)
-        }
-    })
+describe("Timer", () => {
+  it("shows 10 seconds", () => {
+    cy.visit("/");
+    for (let j = 1; j < 10; j++) {
+      cy.contains(".status__time", `00:0${j}`);
+    }
+  });
 
-    it('shows minutes and seconds since the game started', () => {
-        cy.clock()
-        cy.visit('/')
-        cy.contains('.status__time', '00:00')
-        cy.tick(30_000)
-        cy.contains('.status__time', '00:30')
-        cy.tick(30_000)
-        cy.contains('.status__time', '01:00')
-        cy.tick(640_000)
-        cy.contains('.status__time', '11:40')
-
-    })
-})
+  it("shows minutes and seconds since the game started", () => {
+    cy.clock();
+    cy.visit("/");
+    cy.contains(".status__time", "00:00");
+    cy.tick(30_000);
+    cy.contains(".status__time", "00:30");
+    cy.tick(30_000);
+    cy.contains(".status__time", "01:00");
+    cy.tick(640_000);
+    cy.contains(".status__time", "11:40");
+  });
+});
